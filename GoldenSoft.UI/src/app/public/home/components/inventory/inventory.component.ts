a20a1a06438bdb05b0351fc6ccaf822b98c4c2ca
Armando <armandoterancastillo1@outlook.com>
2023-03-07 17:23:52 -0700

    Exportar en Excel y Pdf

--- GoldenSoft.UI/src/app/public/home/components/inventory/inventory.component.ts
+++ GoldenSoft.UI/src/app/public/home/components/inventory/inventory.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { PublicService } from "src/app/public/public.service";
 import {combineLatest, concat, forkJoin, map, merge, of, tap} from "rxjs"
+import { Workbook } from 'exceljs';
+import saveAs from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+import { exportDataGrid as exportDataGridToPdf } from 'devextreme/pdf_exporter';
+import { jsPDF } from 'jspdf';
 
 
 @Component({
@@ -39,6 +44,31 @@ export class InventoryComponent implements OnInit {
         this.getClient();
         this.getPurchaseOrder();
       
+    }
+
+    exportData(e){
+        if (e.format === 'xlsx') {
+            const workbook = new Workbook(); 
+            const worksheet = workbook.addWorksheet("Main sheet"); 
+            exportDataGrid({ 
+                worksheet: worksheet, 
+                component: e.component,
+            }).then(function() {
+                workbook.xlsx.writeBuffer().then(function(buffer) { 
+                    saveAs(new Blob([buffer], { type: "application/octet-stream" }), "DataGrid.xlsx"); 
+                }); 
+            }); 
+            e.cancel = true;
+        }
+        else if (e.format === 'pdf') {
+            const doc = new jsPDF();
+            exportDataGridToPdf({
+                jsPDFDocument: doc,
+                component: e.component,
+            }).then(() => {
+                doc.save('DataGrid.pdf');
+            });
+        }
     }
 
     getInventory(){
